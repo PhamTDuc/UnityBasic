78b3bbcc9505a4e53775a185e147613e63dfa626
Phamduc <phamduc0701@gmail.com>
2020-11-19 01:56:58 +0700

    ! Initilize UnityBasic Project

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+	public Rigidbody rb;
+	public float forward = 500f;
+	public float side = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello the world!!");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forward * Time.deltaTime);
+        if (Input.GetKey("d"))
+        	rb.AddForce(side*Time.deltaTime,0,0);   
+        if (Input.GetKey("a"))
+        	rb.AddForce(-side*Time.deltaTime,0,0);        
+    }
+}
