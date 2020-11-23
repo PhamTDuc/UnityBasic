1e6b811c0d3c1a96f29c17b22534af9594c63bff
PhamDuc <duc.phamtrung@gameloft.com>
2020-11-23 17:02:02 +0700

    ! Basic Gameplay DONE

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+   public void StartNewGame()
+   {
+   		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+}
